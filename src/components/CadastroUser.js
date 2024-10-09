67ace30c72a417e2f80910c5dd37e9de4e56a87a
aamekst <mieko.alana@gmail.com>
2024-10-09 11:12:28 -0300

    'conexao_back'

--- src/components/CadastroUser.js
+++ src/components/CadastroUser.js
@@ -1,58 +1,94 @@
 import './CadastroUser.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
-function CadastroUser(){
-    const [email, SetEmail] = useState('')
-    console.log(email)
+function CadastroUser() {
+    const [email, setEmail] = useState('');
+    const [nome, setNome] = useState('');
+    const [cpfcnpj, setCpfCnpj] = useState('');
+    const [senha, setSenha] = useState('');
 
-    let nome = "Alana";
-    let profissao = "Dev";
+    async function salvar(e) {
+        e.preventDefault();
 
+        // Montar o objeto com os dados do formulário
+        const user = {
+            name: nome,
+            email: email,
+            password: senha,
+            cpfcnpj: cpfcnpj,
+            is_active: true 
+        };
 
+        // Enviar os dados para a API
+        let api = await fetch("http://localhost:8000/api/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
 
-   async function salvar(){
-
-        let api = await fetch("https://viacep.com.br/ws/"+email+"/json/");
         let resposta = await api.json();
 
-        if(api.ok){
-            console.log(resposta)
-            return
+        if (api.ok) {
+            console.log("Usuário cadastrado com sucesso:", resposta);
+            alert("Usuário cadastrado com sucesso. Bem vindo, "+ nome);
+            setNome('');
+            setEmail('');
+            setCpfCnpj('');
+            setSenha('');
+            
+        } else {
+            console.error("Erro ao cadastrar o usuário:", resposta);
+            alert("Erro ao cadastrar usuário.");
         }
-    
-        console.log(resposta);
-        
     }
 
-    return(
+    return (
         <div id="formulario">
-            <form>
+            <form onSubmit={salvar}>
                 <h1>Cadastrar</h1>
+
                 <label htmlFor='nome'>Nome:</label>
-                <input type='text' name='nome' id='nome'/><br></br>
+                <input 
+                    type='text' 
+                    name='nome' 
+                    id='nome'
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                /><br />
 
                 <label htmlFor='email'>E-mail:</label>
-                <input 
-                    type='email' 
-                    name='email' 
+                <input
+                    type='email'
+                    name='email'
                     id='email'
                     value={email}
-                    onChange={(e) => SetEmail(e.target.value)}
-                    
-                    />
+                    onChange={(e) => setEmail(e.target.value)}
+                /><br />
 
                 <label htmlFor='cpfcnpj'>CPF/CNPJ:</label>
-                <input type='text' name='cpfcnpj' id='cpfcnpj'/>
+                <input 
+                    type='text' 
+                    name='cpfcnpj' 
+                    id='cpfcnpj'
+                    value={cpfcnpj}
+                    onChange={(e) => setCpfCnpj(e.target.value)}
+                /><br />
 
-                <label htmlFor='senha' >Senha:</label>
-                <input type='password' name='senha' id='senha'/><br></br>
+                <label htmlFor='senha'>Senha:</label>
+                <input 
+                    type='password' 
+                    name='senha' 
+                    id='senha'
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                /><br />
 
-                <input onClick={salvar} type='button' value="Cadastrar"/>
-                
+                <input type='submit' value="Cadastrar" />
             </form>
-            
         </div>
-    )
-
+    );
 }
+
 export default CadastroUser;
